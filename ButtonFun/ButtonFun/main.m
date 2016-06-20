93cc1afa191071e9c75f3c51b028c693b6f63702
RiversLau <zhaoxiang0805@126.com>
2016-06-20 15:19:54 +0800

    Second Project！实现基本交互！

---
+++ ButtonFun/ButtonFun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ButtonFun
+//
+//  Created by shine-team2 on 16/6/20.
+//  Copyright © 2016年 ShowTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
