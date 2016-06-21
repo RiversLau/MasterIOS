38c186a8e186ebc838a9f037c953525711c5c1ab
RiversLau <zhaoxiang0805@126.com>
2016-06-21 11:56:27 +0800

    完善Second Project

--- ButtonFun/ButtonFun/ViewController.m
+++ ButtonFun/ButtonFun/ViewController.m
@@ -10,18 +10,25 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)buttonPressed:(UIButton *)sender {
+    
+    NSString *title = [sender titleForState: UIControlStateNormal];
+    NSString *plainText = [NSString stringWithFormat: @"%@ button pressed.", title];
+//    _statusLabel.text = plainText;
+    
+    NSMutableAttributedString *styleText = [[NSMutableAttributedString alloc]initWithString:plainText];
+    NSDictionary *attributes = @{NSFontAttributeName : [UIFont boldSystemFontOfSize: _statusLabel.font.pointSize]};
+    NSRange nameRange = [plainText rangeOfString: title];
+    [styleText setAttributes: attributes range: nameRange];
+    
+    _statusLabel.attributedText = styleText;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
